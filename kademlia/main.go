dc9875d533b12f7620283579387530442541a1d8
姚礼涛 <yaolitao@advanpro.hk>
2019-04-09 17:08:24 +0800

    'init'

---
+++ kademlia/main.go
@@ -0,0 +1,25 @@
+package main
+
+// 探测一个节点是否在线
+func Ping() {
+
+}
+
+// 令对方储存一份数据
+func Store() {
+
+}
+
+// 根据节点ID查找一个节点
+func FindNode() {
+
+}
+
+// 根据键查找一个值
+func FindValue() {
+
+}
+
+func main() {
+
+}
