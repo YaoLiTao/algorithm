90dd50040dac25d60191f541d3493ebd45222288
姚礼涛 <yaolitao@advanpro.hk>
2019-07-01 11:40:38 +0800

    '完成直接插入排序'

--- main.go
+++ main.go
@@ -7,14 +7,30 @@ import (
 )
 
 func main() {
+	var data []int
+
+	data = getRandInt()
+	sort.QuickSort(data, 0, len(data)-1)
+	fmt.Println(data)
+
+	data = getRandInt()
+	sort.BubbleSort(data[:])
+	fmt.Println(data)
+
+	data = getRandInt()
+	sort.SelectionSort(data[:])
+	fmt.Println(data)
+
+	data = getRandInt()
+	sort.StraightInsertionSort(data[:])
+	fmt.Println(data)
+}
+
+func getRandInt() []int {
 	data := [100]int{}
 	for i := 0; i < len(data); i++ {
 		data[i] = rand.Intn(1000)
 	}
-	fmt.Println(data)
-	//sort.QuickSort(data[:], 0, len(data)-1)
-	//sort.BubbleSort(data[:])
-	//sort.SelectionSort(data[:])
-	sort.StraightInsertionSort(data[:])
-	fmt.Println(data)
+	fmt.Println("randInt: ", data)
+	return data[:]
 }
--- sort/sort.go
+++ sort/sort.go
@@ -7,6 +7,55 @@ func swap(a *int, b *int) {
 }
 
 /**
+冒泡排序
+*/
+func BubbleSort(data []int) {
+	for i := 0; i < len(data); i++ {
+		for j := 0; j < len(data)-i-1; j++ {
+			if data[j] > data[j+1] {
+				swap(&data[i], &data[j])
+			}
+		}
+	}
+}
+
+/**
+选择排序
+*/
+func SelectionSort(data []int) {
+	for i := 0; i < len(data); i++ {
+		for j := i; j < len(data); j++ {
+			if data[i] > data[j] {
+				swap(&data[i], &data[j])
+			}
+		}
+	}
+}
+
+/**
+直接插入排序
+ */
+func StraightInsertionSort(data []int) {
+	j := 0
+	k := 0
+	for i := 1; i < len(data); i++ {
+		for j = i - 1; j >= 0; j-- {
+			if data[j] < data[i] {
+				break
+			}
+		}
+
+		if j != i-1 {
+			temp := data[i]
+			for k = i - 1; k > j; k-- {
+				data[k+1] = data[k]
+			}
+			data[k+1] = temp
+		}
+	}
+}
+
+/**
 快速排序
 */
 func QuickSort(data []int, l int, r int) {
@@ -37,33 +86,22 @@ func QuickSort(data []int, l int, r int) {
 }
 
 /**
-冒泡排序
-*/
-func BubbleSort(data []int) {
-	for i := 0; i < len(data); i++ {
-		for j := 0; j < len(data)-i-1; j++ {
-			if data[j] > data[j+1] {
-				swap(&data[i], &data[j])
-			}
-		}
-	}
-}
-
-/**
-选择排序
-*/
-func SelectionSort(data []int) {
-	for i := 0; i < len(data); i++ {
-		for j := i; j < len(data); j++ {
-			if data[i] > data[j] {
-				swap(&data[i], &data[j])
-			}
-		}
-	}
-}
-
-/**
-直接插入排序
+归并排序
  */
-func StraightInsertionSort(data []int) {
+func MergeSort(data []int) {
+
+}
+
+/**
+堆排序
+ */
+func HeapSort(data []int) {
+
+}
+
+/**
+希尔排序
+ */
+func ShellsSort() {
+
 }
