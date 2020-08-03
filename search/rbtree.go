45a313a954bb505cd83c68aba566973c633ced31
yaolitao <1738642116@qq.com>
2020-08-03 18:23:30 +0800

    修改红黑树实现

--- search/rbtree.go
+++ search/rbtree.go
@@ -1,9 +1,22 @@
 package search
 
+/**
+1.节点是红色或黑色。
+2.根是黑色。
+3.所有叶子都是黑色（叶子是NIL节点）。
+4.每个红色节点必须有两个黑色的子节点。（从每个叶子到根的所有路径上不能有两个连续的红色节点。）
+5.从任一节点到其每个叶子的所有简单路径都包含相同数目的黑色节点。
+*/
+
 type RBTree struct {
 	size     int64
 	rootNode *node
 }
+
+const (
+	RED   = false
+	BLACK = true
+)
 
 type node struct {
 	data   int
@@ -22,16 +35,15 @@ func NewRBTree() RBTree {
 
 func (tree *RBTree) Insert(data int) {
 	if tree.rootNode == nil {
-		tree.rootNode = &node{
+		tree.rootNode = &node{ // 性质2
 			data:   data,
-			status: true,
+			status: BLACK,
 			parent: nil,
 			left:   nil,
 			right:  nil,
 		}
 		return
 	}
-
 }
 
 func (tree *RBTree) Delete(data int) {
